19142f6951387edc7da1f207ad898fc94f3af7c6
limulim <l22139428@gmail.com>
2023-03-27 21:23:37 +0900

    jquery

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,23 @@
+let index = {
+	init: function(){
+		$("#btn-save").on("click", ()=>{
+			this.save();
+		});
+		
+	},
+	
+	save: function(){
+		//alert('user의 save함수 호출됨');
+		let data = {
+			username: $("#user").val(),
+			password: $("#password").val(),
+			email: $("#email").val()
+		}
+		
+		//console.log(data)
+	
+	  $.ajax().done().fail();
+	}
+}
+
+index.init();
--- src/main/webapp/WEB-INF/views/layout/footer.jsp
+++ src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -6,8 +6,5 @@
 		<p>🔴 010-1234-1234</p>
 		<p>🔴 서울 강남구</p>
 	</div>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
