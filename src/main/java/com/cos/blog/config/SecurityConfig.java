2a8f970adac507abfe9677eae776399dd0f0b7c7
limulim <l22139428@gmail.com>
2023-04-07 13:58:47 +0900

    Security login

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.cos.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.cos.blog.config.auth.PrincipalDetailsService;
+
 @Configuration
 public class SecurityConfig {
-
-	
+		
+	 @Autowired
+	private PrincipalDetailsService principalDetailsService;
 	
 	@Bean
 	BCryptPasswordEncoder encodePWD() {
@@ -17,9 +22,8 @@ public class SecurityConfig {
 	}
 	
 	
-	
-	@Bean 
-	SecurityFilterChain configure(HttpSecurity http) throws Exception{
+	@Bean
+	SecurityFilterChain configure(HttpSecurity http) throws Exception {
 	http
 	.csrf().disable()	
 	.authorizeRequests()
@@ -27,10 +31,13 @@ public class SecurityConfig {
 			.permitAll()
 			.anyRequest()
 			.authenticated()
-			.and()
+		.and()
 			.formLogin()
-			.loginPage("/auth/loginForm");
+			.loginPage("/auth/loginForm")
+			.loginProcessingUrl("/auth/loginProc")
+			.defaultSuccessUrl("/");
 		return http.build();
+
 	}
 	
 }
