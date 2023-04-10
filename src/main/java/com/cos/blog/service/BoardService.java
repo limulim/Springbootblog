47fa5fb070ac865dd6cd070c30f11ebf43d58a42
limulim <l22139428@gmail.com>
2023-04-10 23:39:09 +0900

    delete, modify

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -5,6 +5,8 @@ package com.cos.blog.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -30,8 +32,32 @@ public class BoardService {
 		boardRepository.save(board);
 	}
 	
-	public List<Board> 글목록(){
-	return boardRepository.findAll();
+	@Transactional(readOnly = true)
+	public Page<Board> 글목록(Pageable pageable){
+	return boardRepository.findAll(pageable);
 	}
 	
+	@Transactional(readOnly = true)
+	public Board 글상세보기(int id) {
+		return boardRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("글 상세보기 실패: 아이디를 찾을 수 없습니다.");
+				});
+	}
+	
+	@Transactional
+	public void 글삭제하기(int id) {
+	 	boardRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public void 글수정하기(int id, Board requestBoard) {
+		Board board = boardRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("글 찾기 실패: 아이디를 찾을 수 없습니다.");
+				});
+		board.setTitle(requestBoard.getTitle());
+		board.setContent(requestBoard.getContent());
+	}
+
 }
