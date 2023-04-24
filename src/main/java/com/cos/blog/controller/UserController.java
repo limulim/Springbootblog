38769e7051df62be7100490ae24566bc8a660609
limulim <l22139428@gmail.com>
2023-04-24 23:02:24 +0900

    kakao login+

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -1,7 +1,21 @@
 package com.cos.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import com.cos.blog.model.KakaoProfile;
+import com.cos.blog.model.OAuthToken;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class UserController {
@@ -16,9 +30,82 @@ public class UserController {
 		return "user/loginForm";
 	}
 	
+	
+	@GetMapping("/auth/kakao/callback")
+	public@ResponseBody String kakaoCallback(String code) {
+		RestTemplate rt = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "d3c7a7e6721f191d288ceaebdfeb14f9");
+		params.add("redirect_uri","http://localhost:8000/auth/kakao/callback");
+		params.add("code", code);
+		
+		HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+				new HttpEntity<>(params, headers);
+		
+		ResponseEntity<String> response = rt.exchange(
+				"https://kauth.kakao.com/oauth/token",
+				HttpMethod.POST,
+				kakaoTokenRequest,
+				String.class
+				);
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		OAuthToken oauthToken = null;
+		try {
+			oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("카카오액세스 토큰:" + oauthToken.getAccess_token());
+		
+	 
+		RestTemplate rt2 = new RestTemplate();
+		
+		HttpHeaders headers2 = new HttpHeaders();
+		headers2.add("Authorization", "Bearer "+oauthToken.getAccess_token());
+		headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		
+		HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 =
+				new HttpEntity<>(headers2);
+		
+		ResponseEntity<String> response2 = rt2.exchange(
+				"https://kapi.kakao.com/v2/user/me",
+				HttpMethod.POST,
+				kakaoProfileRequest2,
+				String.class
+				);
+		
+		System.out.println(response2.getBody());
+		
+		ObjectMapper objectMapper2 = new ObjectMapper();
+		KakaoProfile kakaoProfile = null;
+		try {
+			kakaoProfile = objectMapper2.readValue(response2.getBody(), KakaoProfile.class);
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		System.out.println("카카오아이디:" +kakaoProfile.getId());
+		System.out.println("카카오이메일:" +kakaoProfile.getKakao_account().getEmail());
+		
+		
+		return response2.getBody();	
+	}
+		
+
 	@GetMapping("/user/updateForm")
 	public String updateForm() {
 		return "user/updateForm";
 	}
-	
 }
+
--- src/main/webapp/WEB-INF/views/user/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/user/loginForm.jsp
@@ -14,8 +14,10 @@
 			<input type="password" name="password"  class="form-control" placeholder="Enter password" id="password">
 		</div>
 	<button id="btn-login"  class="btn btn-primary">로그인</button>
-	<a href="#"><img height="38px" src="/image/kakao_login_button.png"/></a>
+	<a href="https://kauth.kakao.com/oauth/authorize?client_id=d3c7a7e6721f191d288ceaebdfeb14f9&redirect_uri=http://localhost:8000/auth/kakao/callback&response_type=code">
+	<img height="38px" src="/image/kakao_login_button.png"/></a>
 	</form>
 </div>
 
 <%@ include file="../layout/footer.jsp"%>
+	
